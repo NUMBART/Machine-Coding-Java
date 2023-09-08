77e4812243ffb64848988f35b81d08c6264f3eb9
NUMBART <subhrangshu.pandey.hit@gmail.com>
2023-09-08 22:24:04 +0530

    added test file

---
+++ src/test/java/com/pandey/MainTest.java
@@ -0,0 +1,11 @@
+package com.pandey;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMainMethod() {
+        // This test method is empty
+    }
+}
